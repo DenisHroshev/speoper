ba8946bacc802257a84524b457f099246bad88b5
Danil Makarov <danil.makarov@onit.com>
2024-10-11 17:37:38 +0300

    Added basic routes.

---
+++ src/app/transports/page.tsx
@@ -0,0 +1,13 @@
+export default function TransportssLayout() {
+  return (
+    <>
+      <h1> Vetalik aye </h1>
+      <p>transports</p>
+    </>
+    // <html lang="en">
+    //   <body>
+    //     <h1> Vetalik AYE </h1>
+    //   </body>
+    // </html>
+  );
+}
